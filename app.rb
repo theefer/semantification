cbd2dee33dc0487a9b862460f417a94578cb86c1
Sébastien Cevey <seb@cine7.net>
2012-07-17 11:39:59 +0100

    Dry skeleton of the app

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+
+require 'sinatra'
+require 'erubis'
+
+
+def read_template(name)
+  template = File.read("templates/#{name}.erb")
+  Erubis::Eruby.new(template)
+end
+
+get '/' do
+  # HOMEPAGE
+  template = read_template('welcome')
+  template.result(:who => "You")
+end
+
+get '/story/:id' do
+  id = params[:id]
+  # STORY
+end
+
+get '/event/:id' do
+  id = params[:id]
+  # EVENT
+end
+
+get '/article/:id' do
+  id = params[:id]
+  # ARTICLE
+end
+
+
+# Or an API?
+
+get '/api/story/:id' do
+  # STORY
+end
